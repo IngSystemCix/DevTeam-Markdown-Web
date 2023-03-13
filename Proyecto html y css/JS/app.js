ec37073372b4c6eed3962816b2a5873a367d667c
INGSYSTEM <juanromerocollazos@gmail.com>
2023-03-13 14:09:07 -0500

    Modificado por Juan Romero Collazos

--- Proyecto html y css/JS/app.js
+++
@@ -1,9 +0,0 @@
-addEventListener('DOMContentLoaded', () => {
-  const btn_menu = document.querySelector('.btn_menu')
-  if (btn_menu){
-    btn_menu.addEventListener('click', () => {
-      const menu_items = document.querySelector('.menu_items')
-      menu_items.classList.toggle('show')
-    })
-  }
-})
